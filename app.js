aaaadd299d4271df45ceda704e0dfaca5f55c3b7
Mila Endo <milal.alim@gmail.com>
2017-08-09 09:05:22 -0700

    passport

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const recordsModel = require("./models/records")
+const passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
+
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
@@ -11,6 +15,65 @@ app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.urlencoded({ extended: false }))
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/login/', function(req, res) {
+    res.render("login", {
+    });
+});
+app.post('/login/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login/',
+    failureFlash: true
+}))
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
+const requireLogin = function (req, res, next) {
+  if (req.user) {
+    next()
+  } else {
+    res.redirect('/login/');
+  }
+}
+app.get('/secret/', requireLogin, function (req, res) {
+  res.render("secret");
+})
 
 app.get("/", function(req, res, next){
 	recordsModel.Record.find({})
@@ -83,3 +146,6 @@ app.post("/edit/record", function(req,res,next){
 app.listen(3000, function(){
   console.log("App running on port 3000")
 })
+
+
+// todo add in passport 
